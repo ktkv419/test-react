d115b7bd883f877871917d15ce72cb40ef6fbcae
Pasha <39154112+ktkv419@users.noreply.github.com>
2025-09-24 08:58:32 +0000

    123

--- react-project/vite.config.js
+++ react-project/vite.config.js
@@ -1,7 +1,8 @@
+// react-project/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // <-- replace with your repo name (or '/' for user pages)
 })
